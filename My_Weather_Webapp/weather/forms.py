a0b9f96990ddacdb87747031fff5dd2b8238e119
Kalpana Upadhyay <51444674+Kalpana98@users.noreply.github.com>
2019-06-11 17:00:27 +0530

    adding missing widgets

--- My_Weather_Webapp/weather/forms.py
+++ My_Weather_Webapp/weather/forms.py
@@ -3,4 +3,5 @@ from .models import City
 class CityForm(ModelForm):
     class Meta:
         model = City
-        fields = {'name' : TextInput(attrs={'class' : 'input', 'placeholder' : 'City Name'})}
+        fields = ['name']
+        widgets = {'name' : TextInput(attrs={'class' : 'input', 'placeholder' : 'City Name'})}
