078bde9ec2580aa41d5d2b7b05664e04d546246e
Kalpana Upadhyay <51444674+Kalpana98@users.noreply.github.com>
2020-10-12 20:58:30 +0530

    Update settings.py
    removed secret key

--- My_Weather_Webapp/My_Weather_Webapp/settings.py
+++ My_Weather_Webapp/My_Weather_Webapp/settings.py
@@ -20,8 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=5phn(3l()kaw+qn6@n=%5qh-3!_%x+=4t5m2t8_4)na8f2t4d'
-
+SECRET_KEY = 'secret_key' # sould be unique for you
+ 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
